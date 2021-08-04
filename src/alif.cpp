223e36c9af8e01e3158d76c7079abe52048c55d1
Hassan DRAGA <dragahassan@gmail.com>
2021-08-03 23:47:24 -0300

    Alif 3.0.33

--- src/alif.cpp
+++ src/alif.cpp
@@ -21,7 +21,7 @@
 	<http://www.gnu.org/licenses/>.
 */
 
-#define ALIF_VERSION "3.0.32 (Beta)"
+#define ALIF_VERSION "3.0.33 (Beta)"
 
 // Stack ********************************************************
 
@@ -37,9 +37,13 @@
 // OS Include ***************************************************
 
 	#ifdef _WIN32
+		#define _WINSOCKAPI_
 		#include <winsock2.h>
 		#include <windows.h>
 		#include <tchar.h>
+		#include <direct.h> // getcwd
+		#define GetCurrentDir _getcwd
+		#include <stdlib.h>
 	#elif  __APPLE__
 		// ...
 	#else
@@ -77,6 +81,7 @@
 	#include <boost/program_options.hpp>
 	#include <boost/locale.hpp>
 	#include <boost/lexical_cast.hpp>
+	#include <boost/beast/core/detail/base64.hpp>
 
 	#include <boost/nowide/args.hpp>
 	#include <boost/nowide/fstream.hpp>
@@ -94,7 +99,12 @@
 
 // Global *******************************************************
 
-	// New
+	// Alif v3
+
+	unsigned char * _base64_encode(const unsigned char *src, size_t len, size_t *out_len);
+	unsigned char * _base64_decode(const unsigned char *src, size_t len, size_t *out_len);
+	std::string base64_encode(std::string data);
+	std::string base64_decode(std::string data);
 
 	struct _settings {
 
@@ -412,6 +422,7 @@
 
 	bool ERROR_PRINT_ON_SCREEN = false;
 	bool ERROR_AS_JSON = false;
+	bool ERROR_AS_JSON64 = false;
 
 	// Log
 	std::string LOG_MESSAGE_FULL = "";
@@ -569,11 +580,41 @@
 
 // Log **********************************************************
 
+	void clean_str_json(std::string &s){
+
+		boost::algorithm::replace_all(s, "\r", "");
+		boost::algorithm::replace_all(s, "\n", "");
+		boost::algorithm::trim(s);
+
+		if(ERROR_AS_JSON64){
+
+			//boost::beast::detail::base64::encode((void *)s.c_str(), (void *)s.c_str(), s.length());
+			//size_t len = 0;
+			//_base64_encode((const unsigned char *) s.c_str(), s.length(), &len);
+			s = base64_encode(s);
+			boost::algorithm::replace_all(s, "\r", "");			// remove \r on windows
+			boost::algorithm::replace_all(s, "\n", "");			// remove \n
+			boost::algorithm::replace_all(s, "\\n", "");		// skip auto 72 char break
+		}
+		else {
+
+			boost::algorithm::replace_all(s, "\\", "\\\\");
+			boost::algorithm::replace_all(s, "\"", "\\\"");
+		}
+	}
+
 	void ALIF_ERROR(std::string ERROR_DESCRIPTION) 
 	{
 		cout << endl << ERROR_DESCRIPTION << endl << endl;
 		
 		boost::nowide::ofstream file;
+
+		if(ERROR_AS_JSON){
+
+			clean_str_json(ERROR_DESCRIPTION);
+
+			ERROR_DESCRIPTION = "{\"file\":\"\",\"line\":\"0\",\"msg\":\"" + ERROR_DESCRIPTION + "\"}";
+		}
 		
 		file.open (PATH_FULL_LOG, ios::app);
 			file << ERROR_DESCRIPTION;
@@ -627,7 +668,9 @@
 		if(ERROR_AS_JSON){
 
 			// JSON Format
-			boost::replace_all(error_message, "\"", "\\\"");
+			clean_str_json(error_message);
+			clean_str_json(error_file);
+
 			ERR_MSG = "{\"file\":\"" + error_file + "\",\"line\":\"" + error_line + "\",\"msg\":\"" + error_message + "\"}";
 		}
 		else {
@@ -3611,8 +3654,8 @@
 					if(CLASS_FUN_IS_SET[std::make_pair(TMP_NAMESPACE_OR_CLASS, SYNTAX[p])])
 						ThisIsClassFunction = true;
 					
-					if(ThisIsClassFunction)
-						ErrorCode("_TEST_TEST_TEST_' " + SYNTAX[p + 1] + " ' ", o_tokens);
+					// if(ThisIsClassFunction)
+					// 	ErrorCode("_TEST_TEST_TEST_' " + SYNTAX[p + 1] + " ' ", o_tokens);
 					
 					int TMP_FUN_LONG = p + 2; // a + b + 'p'fun ( c + (1 * 2) ) + c
 					int OPEN_PARENTIZE = 0;
@@ -4996,17 +5039,6 @@
 // Setup ********************************************************
 
 	#ifdef _WIN32
-
-		#include <direct.h> // getcwd (Get current working directory)
-		#define GetCurrentDir _getcwd
-
-		#include <winsock2.h>
-		#include <windows.h> // Get Working Path by Win32 API
-
-		//#include <w32api.h>
-		//#include <Winbase.h>
-		//#include <windows.h> // Get Absolute Path by Win32 API
-		#include <stdlib.h> // Get Temp envirenemt by Win32 API getenv()
 
 		std::string GET_WORKING_PATH_WIN32()
 		{
@@ -8138,6 +8170,162 @@
 		}
 	}
 
+	static const unsigned char base64_table[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+	unsigned char * _base64_encode(const unsigned char *src, size_t len, size_t *out_len){
+
+		unsigned char *out, *pos;
+		const unsigned char *end, *in;
+		size_t olen;
+		int line_len;
+
+		olen = len * 4 / 3 + 4; /* 3-byte blocks to 4-byte */
+		olen += olen / 72; /* line feeds */
+		olen++; /* nul termination */
+		if (olen < len)
+			return NULL; /* integer overflow */
+		out = (unsigned char *)malloc(olen);
+		if (out == NULL)
+			return NULL;
+
+		end = src + len;
+		in = src;
+		pos = out;
+		line_len = 0;
+		while (end - in >= 3) {
+			*pos++ = base64_table[in[0] >> 2];
+			*pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
+			*pos++ = base64_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
+			*pos++ = base64_table[in[2] & 0x3f];
+			in += 3;
+			line_len += 4;
+			if (line_len >= 72) {
+				*pos++ = '\n';
+				line_len = 0;
+			}
+		}
+
+		if (end - in) {
+			*pos++ = base64_table[in[0] >> 2];
+			if (end - in == 1) {
+				*pos++ = base64_table[(in[0] & 0x03) << 4];
+				*pos++ = '=';
+			} else {
+				*pos++ = base64_table[((in[0] & 0x03) << 4) |
+							  (in[1] >> 4)];
+				*pos++ = base64_table[(in[1] & 0x0f) << 2];
+			}
+			*pos++ = '=';
+			line_len += 4;
+		}
+
+		if (line_len)
+			*pos++ = '\n';
+
+		*pos = '\0';
+		if (out_len)
+			*out_len = pos - out;
+		return out;
+	}
+
+	unsigned char * _base64_decode(const unsigned char *src, size_t len, size_t *out_len){
+
+		unsigned char dtable[256], *out, *pos, block[4], tmp;
+		size_t i, count, olen;
+		int pad = 0;
+
+		memset(dtable, 0x80, 256);
+		for (i = 0; i < sizeof(base64_table) - 1; i++)
+			dtable[base64_table[i]] = (unsigned char) i;
+		dtable['='] = 0;
+
+		count = 0;
+		for (i = 0; i < len; i++) {
+			if (dtable[src[i]] != 0x80)
+				count++;
+		}
+
+		if (count == 0 || count % 4)
+			return NULL;
+
+		olen = count / 4 * 3;
+		pos = out = (unsigned char *)malloc(olen);
+		if (out == NULL)
+			return NULL;
+
+		count = 0;
+		for (i = 0; i < len; i++) {
+			tmp = dtable[src[i]];
+			if (tmp == 0x80)
+				continue;
+
+			if (src[i] == '=')
+				pad++;
+			block[count] = tmp;
+			count++;
+			if (count == 4) {
+				*pos++ = (block[0] << 2) | (block[1] >> 4);
+				*pos++ = (block[1] << 4) | (block[2] >> 2);
+				*pos++ = (block[2] << 6) | block[3];
+				count = 0;
+				if (pad) {
+					if (pad == 1)
+						pos--;
+					else if (pad == 2)
+						pos -= 2;
+					else {
+						/* Invalid padding */
+						if(out)
+							free(out);
+						return NULL;
+					}
+					break;
+				}
+			}
+		}
+
+		*out_len = pos - out;
+		return out;
+	}
+
+	std::string base64_encode(std::string data){
+
+		// Encode
+
+		std::string buf			= "";
+		size_t len				= 0;
+		unsigned char * pbuf	= NULL;
+
+		pbuf = _base64_encode((const unsigned char *) data.c_str(), data.length(), &len);
+		buf = reinterpret_cast<char*>(pbuf);
+		//alifcore_trim(buf);
+
+		//alifcore_free(pbuf);
+		
+		//std::cout << "Encode: [" << data << "] -> [" << buf << "] (" << len << " Bytes) \n";
+
+		return buf;
+	}
+
+	std::string base64_decode(std::string data){
+
+		// Decode
+
+		std::string buf			= "";
+		size_t len				= 0;
+		unsigned char * pbuf	= NULL;
+
+		pbuf = _base64_decode((const unsigned char *) data.c_str(), data.length(), &len);
+		buf = reinterpret_cast<char*>(pbuf);
+		//alifcore_trim(buf);
+
+		//alifcore_free(pbuf);
+		
+		//std::cout << "Decode: [" << data << "] -> [" << buf << "] (" << len << " Bytes) \n";
+
+		return buf;
+	}
+
 // Main *********************************************************
 
 	// void alif() {
@@ -8172,8 +8360,13 @@
 			set_output_file(argument.input.path + argument.input.filename + settings.os.exe_ext);
 		
 		// Check log
-		if(argument.log.fullpath == "")
-			set_log_file(argument.input.path + argument.input.filename + argument.input.extension + ".log");
+		if(argument.log.fullpath == ""){
+
+			if(ERROR_AS_JSON)
+				set_log_file(argument.input.path + argument.input.filename + argument.input.extension + ".json");
+			else
+				set_log_file(argument.input.path + argument.input.filename + argument.input.extension + ".log");
+		}
 
 		// Beta test log
 		// cout << "argument.input.path: " << argument.input.path << endl;
@@ -8398,6 +8591,7 @@
 				("o", boost::program_options::value<std::string>(), "Set output file")
 				("log", boost::program_options::value<std::string>(), "Set log file")
 				("log-json", boost::program_options::value<std::string>(), "Set log file as JSON format")
+				("log-json-base64", boost::program_options::value<std::string>(), "Like 'log-json' but data is base64 encoded")
 				("input", 			boost::program_options::value<vector<std::string>>(), "Set input file (only one)")
 				("include-path,I",	boost::program_options::value<vector<std::string>>(), "Add include path")
 				("clib,L",	boost::program_options::value<vector<std::string>>(), "Add extra C/C++ libs")
@@ -8469,11 +8663,20 @@
 				set_log_file(vm["log"].as<std::string>());
 			}
 
-			// Log
+			// Log JSON
 			if (vm.count("log-json")) {
 
 				ERROR_AS_JSON = true;
+				ERROR_AS_JSON64 = false;
 				set_log_file(vm["log-json"].as<std::string>());
+			}
+
+			// Log JSON b64
+			if (vm.count("log-json-base64")) {
+
+				ERROR_AS_JSON = true;
+				ERROR_AS_JSON64 = true;
+				set_log_file(vm["log-json-base64"].as<std::string>());
 			}
 
 			// debug
